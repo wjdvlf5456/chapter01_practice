ba8a9b4f960ff44f1aeee214644fb8137fe4134e
최정필 <wjdvlf1407@gmail.com>
2022-04-13 17:24:34 +0900

    1.21 자료형-문자형 연습2

--- src/com/javaex/ex01/Ex03.java
+++ src/com/javaex/ex01/Ex03.java
@@ -7,7 +7,6 @@ public class Ex03 {
 		/*
 		문자형에는 char 을 사용하는데 character 의 준말이다.
 		char 을 사용할 때에는 한 글자씩만 사용하여야 하고 ''안에 글자를 넣어야 한다.
-		ex) char myTxt = '안';
 		 */
 		
 		char myTxt1 = 'B';
@@ -15,5 +14,10 @@ public class Ex03 {
 		System.out.println(myTxt1);
 		System.out.println(myTxt);
 		
+		/*
+		그리고 char은 정해져 았는 코드로도 사용할 수 있는데 이 때 유의해야 할 것은 문자사이에 ''를 사용하지 않는다.
+		 */
+		char myTxt2 = 65;
+		System.out.println(myTxt2);
 	}
 }
