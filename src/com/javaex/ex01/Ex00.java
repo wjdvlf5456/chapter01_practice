9eb135cc81945194b5420e705892d4e53252c67e
최정필 <wjdvlf1407@gmail.com>
2022-04-13 17:50:34 +0900

    1.4 자료형 String 정리 및 Java폴더 생성 정리추가

---
+++ src/com/javaex/ex01/Ex00.java
@@ -0,0 +1,22 @@
+package com.javaex.ex01;
+
+public class Ex00 {
+	//java 파일 생성 방법
+	public static void main (String[] args) {
+		
+		/*
+		1. Package Explorer창에서 우클릭 New ➡ ︎Java Project 클릭 후 이름 지정
+		 
+		2. 생성한 ︎Java Project 우클릭 이 후 생성된 src폴더 우클릭 후 New ➡ Package 선택한 후 source folder칸과 name 칸이 나오는데 
+		   source folder칸은 패키지명으로 무조건 소문자로 작성하여야 하고 
+		   name칸은 도메인을 거꾸로 쓰고 마지막에 만들고 싶은 프로그램 관련명을 써준다.
+		   ex) 수학관련된 프로그램을 만들면 com.facebook.math 
+		   
+		3. 그 후 생성된 Package 폴더를 우클릭 한 후 New ➡ Class 를 선택하는데 이때 주의해야 하는 것이 첫 글자는 무조건 대문자로 만든 후 .java를 붙여준다.
+		
+		4. class를 새로 생성했으면 public class 옆에 class 명이 제대로 나오는지 확인 후 
+		  아래 요소로 public static void main (String[] args) {} 를 넣어준 후 {} 안에 변수들을 넣어 작업하면 된다.
+		*/
+		
+	}
+}
---
+++ src/com/javaex/ex01/Ex05.java
@@ -0,0 +1,16 @@
+package com.javaex.ex01;
+	//자료형 연습-예외(String)
+public class Ex05 {
+	public static void main (String[] args) {
+		
+		/*
+		String : 원래 자료형이 아니지만 자주 사용되어 자료형이라 취급하고 사용하는 변수
+		원하는 문장을 넣고 싶을 때 사용하며 String txt01 = "";에서 "" 안에 원하는 문장을 넣어 사용한다.
+		
+		!주의: String을 사용할 때는 String의 맨 앞s를 무조건 대문자 S 로 사용해주어야 한다.
+		 */
+		
+		String txt01 = "String연습하는 중입니다.";
+		System.out.println(txt01);
+	}
+}
