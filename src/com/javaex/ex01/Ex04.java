4e62b1083eee22efdb8eabf8a1248f7e521d8a1c
최정필 <wjdvlf1407@gmail.com>
2022-04-13 17:30:34 +0900

    1.3 자료형-논리형 연습

---
+++ src/com/javaex/ex01/Ex04.java
@@ -0,0 +1,19 @@
+package com.javaex.ex01;
+
+public class Ex04 {
+	//자료형-논리형 연습
+	public static void main (String[] args) {
+		/*
+		논리형에는 boolean이라는 변수를 사용하는데 결과값은 true 아니면 false로 나온다.
+		boolean을 사용할 때에는 다른 변수를 같이 사용해야 결과를 얻을 수 있다.
+		*/
+		
+		
+		int var01 = 34;
+		int var02 = 25;
+		
+		boolean myTf = var01>var02;
+		System.out.println(myTf);
+		
+	}
+}
