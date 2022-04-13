343db9d1c3b9dc53543e1e124a0e1ac4bdc12de9
최정필 <wjdvlf1407@gmail.com>
2022-04-13 16:33:32 +0900

    1.0 자료형-정수형 연습

---
+++ src/com/javaex/ex01/Ex01.java
@@ -0,0 +1,22 @@
+package com.javaex.ex01;
+
+public class Ex01 {
+//	자료형 연습
+	public static void main (String[] args) {
+//		sout 입력 후 자동완성을 누르면 System.out.println(); 을 입력할 수 있다.
+		
+		
+		byte var01 = 127;
+		System.out.println(var01);
+		
+		
+		//int는 4byte 크기이며 8자리까지 표현이 가능하다.
+		int myUnit = 1234567891;
+		System.out.println(myUnit);
+		
+		long var12 = 1234567890123456l;
+		System.out.println(var12);
+		
+		
+	}
+}
